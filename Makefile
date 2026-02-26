846119b40e955e606a317f53027581657a800924
Brian Denton <mrblarg64@users.noreply.github.com>
2026-02-25 22:03:04 -0600

    Print tcp info at end of transfer, work towards supporting older versions of Windows.

--- Makefile
+++ Makefile
@@ -4,13 +4,37 @@ CFLAGS += -Wall -Wextra -march=native -Ofast -pipe -I../include -flto -fuse-link
 LDFLAGS += -lgnutls -march=native -Ofast -pipe -flto -fuse-linker-plugin
 
 #default: berntlst.exe
-#CC = x86_64-w64-mingw32-gcc
-#STRIP = x86_64-w64-mingw32-strip
-##CFLAGS += -mconsole -DWINVER=0x0502 -D_WIN32_WINNT=0x0502 -Wall -Wextra -Ofast -pipe -I/home/brian/Documents/brisrc/win32-server-2003-newer/include
-##LDFLAGS += -static -mconsole -Ofast -pipe -lqwave
-######################
-#CFLAGS += -mconsole -DWINVER=0x0600 -D_WIN32_WINNT=0x0600 -Wall -Wextra -Ofast -pipe -I/home/brian/Documents/brisrc/win32-vista-newer/include
-#LDFLAGS += -static -mconsole -Ofast -pipe -lqwave
+#BINPREFIX = i686-w64-mingw32-
+#BINPREFIX = x86_64-w64-mingw32-
+#CC = $(BINPREFIX)gcc
+#STRIP = $(BINPREFIX)strip
+#Windows 10 19H1 newer
+#CFLAGS += -DNTDDI_VERSION=NTDDI_WIN10_19H1 -DWINVER=0x0A00 -D_WIN32_WINNT=0x0A00 -mconsole -Wall -Wextra -Ofast -pipe -I/home/brian/Documents/brisrc/win32-vista-newer/include
+#GNUTLS_PATH = /home/brian/Documents/brisrc/win32-vista-newer/lib/libgnutls.la
+#CFLAGS += -DNTDDI_VERSION=NTDDI_WIN10_19H1 -DWINVER=0x0A00 -D_WIN32_WINNT=0x0A00 -mconsole -Wall -Wextra -Ofast -pipe -I/home/brian/Documents/brisrc/win32-vista-newer-i686/include
+#GNUTLS_PATH = /home/brian/Documents/brisrc/win32-vista-newer-i686/lib/libgnutls.la
+#LDFLAGS += -mconsole -Ofast -pipe -lqwave
+########################
+#Windows Vista newer
+#CFLAGS += -DWINVER=0x0600 -D_WIN32_WINNT=0x0600 -mconsole -Wall -Wextra -Ofast -pipe -I/home/brian/Documents/brisrc/win32-vista-newer/include
+#GNUTLS_PATH = /home/brian/Documents/brisrc/win32-vista-newer/lib/libgnutls.la
+#CFLAGS += -DWINVER=0x0600 -D_WIN32_WINNT=0x0600 -mconsole -Wall -Wextra -Ofast -pipe -I/home/brian/Documents/brisrc/win32-vista-newer-i686/include
+#GNUTLS_PATH = /home/brian/Documents/brisrc/win32-vista-newer-i686/lib/libgnutls.la
+#LDFLAGS += -mconsole -Ofast -pipe -lqwave
+########################
+#Windows Server 2003 newer
+#CFLAGS += -DWINVER=0x0502 -D_WIN32_WINNT=0x0502 -mconsole -Wall -Wextra -Ofast -pipe -I/home/brian/Documents/brisrc/win32-server-2003-newer/include
+#GNUTLS_PATH = /home/brian/Documents/brisrc/win32-server-2003-newer/lib/libgnutls.la
+#CFLAGS += -DWINVER=0x0502 -D_WIN32_WINNT=0x0502 -mconsole -Wall -Wextra -Ofast -pipe -I/home/brian/Documents/brisrc/win32-server-2003-newer-i686/include
+#GNUTLS_PATH = /home/brian/Documents/brisrc/win32-server-2003-newer-i686/lib/libgnutls.la
+#LDFLAGS += -mconsole -Ofast -pipe
+########################
+#NT 4 - 95, NT 3?
+#CFLAGS += -DWINVER=0x0400 -D_WIN32_WINNT=0x0400 -mconsole -Wall -Wextra -Ofast -pipe -I/home/brian/Documents/brisrc/win32-nt-4-newer-i686/include
+#GNUTLS_PATH = /home/brian/Documents/brisrc/win32-nt-4-newer-i686/lib/libgnutls.la
+#LDFLAGS += -mconsole -Ofast -pipe
+
+
 
 
 SRCS = $(wildcard src/*.c)
@@ -23,7 +47,7 @@ windows: berntlst.exe
 linux: berntlst
 
 berntlst.exe: $(OBJS)
-	libtool --tag=CC --mode=link $(CC) $(OBJS) $(LDFLAGS) -o berntlst.exe /home/brian/Documents/brisrc/win32-vista-newer/lib/libgnutls.la
+	libtool --tag=CC --mode=link $(CC) $(OBJS) $(LDFLAGS) -o berntlst.exe $(GNUTLS_PATH)
 	$(STRIP) berntlst.exe
 
 berntlst: $(OBJS)
--- src/main.c
+++ src/main.c
@@ -11,14 +11,12 @@
 #include <qos2.h>
 #else
 #include <mstcpip.h>
-#include <ntdef.h>
-#include <ntstatus.h>
 #endif
 
 #define ERRNO_T DWORD
 #define SOCKET_T SOCKET
 #define NEWLINE "\r\n"
-#define U64_PF "%llu"
+#define U64_PF "%" PRIu64
 #define SSO_CAST (const char*)
 
 #if (WINVER >= 0x0600)
@@ -61,12 +59,13 @@ WSADATA wsd;
 #include <unistd.h>
 #include <locale.h>
 #include <sched.h>
+#include <linux/tcp.h>
 #include <errno.h>
 
 #define ERRNO_T int
 #define SOCKET_T int
 #define NEWLINE "\n"
-#define U64_PF "%'lu"
+#define U64_PF "%'" PRIu64
 #define SSO_CAST (void*)
 #define FASTPRINT(str) ((void)!write(STDOUT_FILENO, str, sizeof(str) - 1))
 
@@ -90,6 +89,7 @@ int fd;
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <stdio.h>
 
 #include <gnutls/gnutls.h>
@@ -194,6 +194,39 @@ static inline void setupsockstorage(struct sockaddr_storage *sas, uint32_t i, ui
 	#endif
 
 	return;
+}
+
+static inline void printtcpinfo()
+{
+	#ifdef _WIN32
+	#if NTDDI_VERSION >= NTDDI_WIN10_RS5
+	DWORD version = 1;
+	TCP_INFO_v1 tcpi;
+	DWORD returned;
+	int wsaerrno;
+
+	if (WSAIoctl(sock, SIO_TCP_INFO, &version, sizeof(DWORD), &tcpi, sizeof(TCP_INFO_v1), &returned, NULL, NULL))
+	{
+		SOCKERROR("WSAIoctl(SIO_TCP_INFO)");
+	}
+
+	printf(NEWLINE "TCP Info" NEWLINE "\t\trtt = %lu" NEWLINE "\t\trtt-min = %lu" NEWLINE "\t\tcwnd = %lu" NEWLINE "\t\trx-bytes = %llu" NEWLINE "\t\ttx-bytes = %llu" NEWLINE "\t\tretrans-bytes = %lu" NEWLINE "\t\tfast-retrans calls = %lu" NEWLINE "\t\tmss = %lu" NEWLINE "\t\treordered-bytes = %lu" NEWLINE "\t\tdup acks recvd = %lu" NEWLINE NEWLINE, tcpi.RttUs, tcpi.MinRttUs, tcpi.Cwnd, tcpi.BytesIn, tcpi.BytesOut, tcpi.BytesRetrans, tcpi.FastRetrans, tcpi.Mss, tcpi.BytesReordered, tcpi.DupAcksIn);
+
+	#endif
+	#else
+	struct tcp_info tcpi;
+	socklen_t optlen;
+	int myerrno;
+
+	optlen = sizeof(struct tcp_info);
+	if (getsockopt(sock, IPPROTO_TCP, TCP_INFO, &tcpi, &optlen))
+	{
+		myerrno = errno;
+		perror("IPPROTO_TCP TCP_INFO getsockopt()");
+		_exit(myerrno);
+	}
+	printf(NEWLINE "TCP Info" NEWLINE "\t\trtt = %'u" NEWLINE "\t\trtt-min = %'u" NEWLINE "\t\trtt-var = %'u" NEWLINE "\t\tcwnd = %'u" NEWLINE "\t\tpacing rate = %'llu" NEWLINE "\t\tmax pacing rate = %'llu" NEWLINE "\t\trx-bytes = %'llu" NEWLINE "\t\ttx-bytes = %'llu" NEWLINE "\t\tnotsent-bytes = %'u" NEWLINE "\t\tsegs out = %'u" NEWLINE "\t\tretrans-pkts = %'u" NEWLINE "\t\tsmss = %u" NEWLINE "\t\trmss = %u" NEWLINE "\t\tpmtu %u" NEWLINE "\t\treorder-events = %'u" NEWLINE NEWLINE, tcpi.tcpi_rtt, tcpi.tcpi_min_rtt, tcpi.tcpi_rttvar, tcpi.tcpi_snd_cwnd, tcpi.tcpi_pacing_rate, tcpi.tcpi_max_pacing_rate, tcpi.tcpi_bytes_received, tcpi.tcpi_bytes_sent, tcpi.tcpi_notsent_bytes, tcpi.tcpi_segs_out, tcpi.tcpi_total_retrans, tcpi.tcpi_snd_mss, tcpi.tcpi_rcv_mss, tcpi.tcpi_pmtu, tcpi.tcpi_reord_seen);
+	#endif
 }
 
 static inline void setupsocket()
@@ -444,12 +477,42 @@ static inline void setupfileinput(const char *const fstr)
 	#endif
 }
 
+#ifdef _WIN32
+static inline void setfp(HANDLE fd, DWORD movmeth, uint64_t offset)
+{
+	LARGE_INTEGER li;
+	DWORD myerrno;
+
+	li.QuadPart = offset;
+	#if (WINVER >= 0x0500)
+	if (!SetFilePointerEx(fd, li, NULL, movmeth))
+	{
+		myerrno = GetLastError();
+		printf("SetFilePointerEx() failed ecode 0x%lx" NEWLINE, myerrno);
+		ExitProcess(myerrno);
+	}
+	#else
+	DWORD retval;
+
+	retval = SetFilePointer(fd, li.LowPart, &li.HighPart, movmeth);
+	if (retval == INVALID_SET_FILE_POINTER)
+	{
+		myerrno = GetLastError();
+		if (myerrno != NO_ERROR)
+		{
+			printf("SetFilePointer() failed ecode 0x%lx" NEWLINE, myerrno);
+			ExitProcess(myerrno);
+		}
+	}
+	#endif
+}
+#endif
+
 static inline void setupfileoutput(const char *const fstr)
 {
 	ERRNO_T myerrno;
 
 	#ifdef _WIN32
-	LARGE_INTEGER li;
 
 	fd = CreateFile(fstr, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
 	if (fd == INVALID_HANDLE_VALUE)
@@ -461,28 +524,14 @@ static inline void setupfileoutput(const char *const fstr)
 
 	if (size)
 	{
-		li.QuadPart = size;
-		if (!SetFilePointerEx(fd, li, NULL, FILE_BEGIN))
-		{
-			myerrno = GetLastError();
-			printf("SetFilePointerEx() failed ecode 0x%lx" NEWLINE, myerrno);
-			ExitProcess(myerrno);
-		}
-
+		setfp(fd, FILE_BEGIN, size);
 		if (!SetEndOfFile(fd))
 		{
 			myerrno = GetLastError();
 			printf("SetEndOfFile() failed ecode 0x%lx" NEWLINE, myerrno);
 			ExitProcess(myerrno);
 		}
-
-		li.QuadPart = 0;
-		if (!SetFilePointerEx(fd, li, NULL, FILE_BEGIN))
-		{
-			myerrno = GetLastError();
-			printf("SetFilePointerEx() failed ecode 0x%lx" NEWLINE, myerrno);
-			ExitProcess(myerrno);
-		}
+		setfp(fd, FILE_BEGIN, 0);
 	}
 	#else
 	fd = open(fstr, O_WRONLY | O_EXCL | O_CREAT, 0644);
@@ -531,7 +580,11 @@ void setuppipe(int argc, char *argv[])
 	}
 
 	cargs.flags = CLONE_PIDFD;
+	#if __WORDSIZE == 64
 	cargs.pidfd = (__u64)&pidfd;
+	#else
+	cargs.pidfd = (__u32)&pidfd;
+	#endif
         p = syscall(SYS_clone3, &cargs, sizeof(struct clone_args));
 	if (p < 0)
 	{
@@ -596,7 +649,7 @@ static inline void spipe()
 	siginfo_t si;
 	ssize_t retval;
 	ssize_t gnutlsretval;
-	ssize_t totsent = 0;
+        uint64_t totsent = 0;
 
 	size = 0;
 	gnutlsretval = my_send_waitall(session, &size, sizeof(uint64_t));
@@ -638,6 +691,7 @@ static inline void spipe()
 				gnutlsretval = gnutls_bye(session, GNUTLS_SHUT_RDWR);
 			}
 			while (gnutlsretval == GNUTLS_E_AGAIN);
+			printtcpinfo();
 			CLOSESOCK(sock);
 			_exit(0);
 		}
@@ -699,11 +753,8 @@ static inline void setupaddress(const char *const addr, const char *const ports)
 	char *endptr;
 
 	#if ((defined(_WIN32)) && (WINVER < 0x0600))
-	char *term;
-	NTSTATUS retval;
-
-	retval = RtlIpv4StringToAddress(addr, 1, &term, (IN_ADDR *) &ip);
-	if ((retval != STATUS_SUCCESS) || (*term != 0))
+	ip = inet_addr(addr);
+	if ((ip == INADDR_NONE) || (ip == INADDR_ANY))
 	{
 		FASTPRINT("Failed to parse ipv4 address" NEWLINE);
 	        exitbadusage();
@@ -867,7 +918,7 @@ static inline void rfile(const char *const fname)
 	ERRNO_T myerrno;
 	char *buf;
 	ssize_t gnutlsretval;
-	size_t totrecv = 0;
+        uint64_t totrecv = 0;
 	#ifdef _WIN32
 	//HANDLE fmap;
 	//FILE_DISPOSITION_INFO fdi = 0;
@@ -988,6 +1039,7 @@ static inline void rfile(const char *const fname)
 		printf("gnutls_bye(): %s" NEWLINE, gnutls_strerror_name(gnutlsretval));
 		FASTEXIT(1);
 	}
+	printtcpinfo();
 	CLOSESOCK(sock);
 	#ifdef _WIN32
 	/* if (!UnmapViewOfFile(buf)) */
@@ -1085,6 +1137,7 @@ static inline void sfile()
 		printf("gnutls_bye(): %s" NEWLINE, gnutls_strerror_name(gnutlsretval));
 		FASTEXIT(1);
 	}
+	printtcpinfo();
 	CLOSESOCK(sock);
         CLOSEFILE(fd);
 	#ifdef _WIN32
